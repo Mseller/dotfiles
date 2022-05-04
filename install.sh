0a860e7872dcaded9af57b794f9d065c6e924a69
martin.seller@schibsted.com <martin.seller@schibsted.com>
2022-05-04 16:26:50 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo "unsupported os type"
-  exit 1
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "unsupported os type"
   exit 1
 fi
 
-
+# Install programs/utils
 brew install stow
 brew install --cask kitty
 brew install --cask karabiner-elements
 brew install fzf
 $(brew --prefix)/opt/fzf/install # Installs useful key bindings and fuzzy completion
 brew install go
+
+# Symlink dot files with stow
+stow kitty
+stow nvim
+stow karbiner
