02842b75563731290d8ff21199bea057874e37d0
martin.seller@schibsted.com <martin.seller@schibsted.com>
2024-05-16 13:54:47 +0200

    Added bin folder

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ fi
 # Install programs/utils
 brew install stow
 brew install --cask kitty
+brew install --cask alacritty
 brew install --cask karabiner-elements
 brew install fzf
 $(brew --prefix)/opt/fzf/install # Installs useful key bindings and fuzzy completion
@@ -17,7 +18,10 @@ brew install go
 
 # Symlink dot files with stow
 stow kitty
+stow alacritty
 stow nvim
 stow tmux
 stow karbiner
+
+ln -s ./bin/* /usr/local/bin
 
